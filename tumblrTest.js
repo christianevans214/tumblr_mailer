2d98273da433afa85f0a6350acc2fa5e2e6725a2
Christian Evans <christian.evans214@gmail.com>
2015-05-09 13:55:24 -0700

    Update tumblrTest.js

--- tumblrTest.js
+++ tumblrTest.js
@@ -2,10 +2,10 @@ var tumblr = require('tumblr.js');
 // Authenticate via OAuth
 var tumblr = require('tumblr.js');
 var client = tumblr.createClient({
-  consumer_key: 'Gvsni1sh0FRDrcbQcHZX106IK948nsVyN4JIEKc1ClqZ8cHVgG',
-  consumer_secret: 'T6LbF17i6e9oGFDEEpOiZpHrCoYcyzpVbUAeKb9sunFATINwcY',
-  token: 'MFjRxFyYQGtSLwShPOUNiXwzOqwpF6G7cGkxJwef7wwbLnld3n',
-  token_secret: 'c1KkpYe7mcpfQSxdxsqqa3Nf5ERH2Zrjek6rWr83cGAFtIF9mf'
+  consumer_key: 'XXXXXXX',
+  consumer_secret: 'XXXXXXXXX',
+  token: 'XXXXXXXXXX',
+  token_secret: 'XXXXXXXXXX'
 });
 
 client.posts( 'antiquecodeshow.tumblr.com', function(err, blog) {
